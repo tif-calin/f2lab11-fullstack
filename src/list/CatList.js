d107fbec0a3437968dbd42480ef54871c0dd8b61
tif-calin <culitif@tuta.io>
2021-05-17 08:00:36 -0700

    implement famous cat API utils

--- src/list/CatList.js
+++ src/list/CatList.js
@@ -4,11 +4,15 @@ import './CatList.css';
 
 export default class CatList extends Component {
 
+  async componentDidMount() {
+    
+  }
+
   render() {
     return (
       <div className="CatList">
         <ul>
-          
+
         </ul>
 
         <Link to='/'>&#9664; back</Link>
---
+++ src/utils/famous-cats-api.js
@@ -0,0 +1,28 @@
+import request from 'superagent';
+
+const URL = '/api/cats';
+
+export async function getCats() {
+  const response = await request.get(URL);
+  return response.body;
+}
+
+export async function getCat(id) {
+  const response = await request.get(`${URL}/${id}`);
+  return response.body;
+}
+
+export async function addCat(cat) {
+  const response = await request.post(URL).send(cat);
+  return response.body;
+}
+
+export async function removeCat(id) {
+  const response = await request.delete(`${URL}/${id}`);
+  return response.body;
+}
+
+export async function updateCat(cat) {
+  const response = await request.put(`${URL}/${cat.id}`).send(cat);
+  return response.body;
+}
