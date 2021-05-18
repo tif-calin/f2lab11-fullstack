adfaeb5ca74adf1297bdb78259b9a20326262724
tif-calin <culitif@tuta.io>
2021-05-17 21:55:53 -0700

    set up inputs and html for AddPage

--- src/add/AddPage.js
+++ src/add/AddPage.js
@@ -4,14 +4,46 @@ import './AddPage.css';
 
 export default class AddPage extends Component {
   state = {
-    cats: []
+    name: '',
+    type: '',
+    url: '',
+    year: 0,
+    lives: 0,
+    isSidekick: false
   };
 
   render() {
     return (
-      <div className="AddPage wrapper-v">
-
-      </div>
+      <form className="AddPage wrapper-v">
+        <h2 className="page-title">Add a cat</h2>
+        <fieldset>
+          <label>
+            name:
+            <input type="text" required/>
+          </label>
+          <label>
+            type:
+            <input type="text" required/>
+          </label>
+          <label>
+            image:
+            <input type="text" required/>
+          </label>
+          <label>
+            year:
+            <input type="number"/>
+          </label>
+          <label>
+            lives:
+            <input type="number" min="0" max="9" placeholder="9"/>
+          </label>
+          <label>
+            sidekick?:
+            <input type="checkbox"/>
+          </label>
+        </fieldset>
+        <button type="submit">Add Cat</button>
+      </form>
     );
   }
 
